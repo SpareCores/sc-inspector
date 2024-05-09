895dbc0a11b79e63507c7ee54883f9ddc6d50cdd
bra-fsn <nagy.attila@gmail.com>
2024-05-09 23:16:36 +0200

    synchronized/use auth if specified

--- inspector/repo.py
+++ inspector/repo.py
@@ -1,25 +1,41 @@
 import os
 import git
 import functools
+from urllib.parse import urlparse, urlunparse
+from wrapt import synchronized
 
+
+# pre-set in the ghcr.io/sparecores/sc-inspector:main image
 REPO_URL = os.environ.get("REPO_URL")
 REPO_PATH = os.environ.get("REPO_PATH")
 
 
+def add_token_auth(url: str, token: str) -> str:
+    parsed = urlparse(url)
+    # works no matter if the original url had a user/pass or not
+    domain = parsed.netloc.split("@")[-1]
+    domain = f"{token}@{domain}"
+    unparsed = (parsed[0], domain, parsed[2], parsed[3], parsed[4], parsed[5])
+    return urlunparse(unparsed)
+
+
 @functools.cache
-def get_repo():
+@synchronized
+def get_repo(repo_url=REPO_URL, repo_path=REPO_PATH):
     """
     Return git.Repo.
 
     If there's an already existing repo, use that, otherwise do a clone.
     """
+    if token := os.environ.get("GITHUB_TOKEN"):
+        repo_url = add_token_auth(repo_url, token)
     try:
-        repo = git.Repo(REPO_PATH)
-    except git.InvalidGitRepositoryError:
-        repo = git.Repo.clone_from(REPO_URL, REPO_PATH)
-    return repo
+        return git.Repo(repo_path)
+    except (git.InvalidGitRepositoryError, git.NoSuchPathError):
+        return git.Repo.clone_from(repo_url, repo_path)
 
 
+@synchronized
 def push_path(path: str | os.PathLike, msg: str):
     repo = get_repo()
     repo.index.add(path)
