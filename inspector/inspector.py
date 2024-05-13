378e4ff38355cbc57bbd2d14a6adbf82377fc8cb
bra-fsn <nagy.attila@gmail.com>
2024-05-13 10:37:52 +0200

    Use default key

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -99,7 +99,7 @@ def start(ctx, exclude, start_only):
         ).decode("ascii")
         # get default instance opts for the vendor and add ours
         instance_opts = default(getattr(sc_runner.resources, vendor).DEFAULTS, "instance_opts")
-        instance_opts |= dict(user_data_base64=b64_user_data)
+        instance_opts |= dict(user_data_base64=b64_user_data, key_name="spare-cores")
         runner.create(vendor, {}, RESOURCE_OPTS.get(vendor) | dict(instance=server, instance_opts=instance_opts))
 
 
