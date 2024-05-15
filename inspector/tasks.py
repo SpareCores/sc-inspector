cde96d027575dd621a06c687578ea18745ef63fd
bra-fsn <nagy.attila@gmail.com>
2024-05-15 12:27:17 +0200

    comment out for now

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -1,9 +1,10 @@
 import lib
-import parse
+# import parse
 
 
 class DmiDecode(lib.DockerTask):
     parallel: bool = True
     priority: int = 0
     image: str = "ghcr.io/sparecores/dmidecode:main"
+    # parse_output: list = [parse.dmidecode]
     command: str = "dmidecode"
