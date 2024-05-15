76169323a7b5ded466308fdadff2b06cb9ebd21a
bra-fsn <nagy.attila@gmail.com>
2024-05-15 12:24:34 +0200

    Use our own (more recent)

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -1,8 +1,9 @@
 import lib
+import parse
 
 
 class DmiDecode(lib.DockerTask):
     parallel: bool = True
     priority: int = 0
-    image: str = "shuuji3/dmidecode"
+    image: str = "ghcr.io/sparecores/dmidecode:main"
     command: str = "dmidecode"
