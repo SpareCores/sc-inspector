9a26e5d647377ff54530f8870c51d753d58c6aaf
bra-fsn <nagy.attila@gmail.com>
2024-05-18 00:16:23 +0200

    Text compression

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -32,3 +32,10 @@ class Nvidia_Smi(lib.DockerTask):
     image: str = "nvidia/cuda:12.4.1-base-ubuntu22.04"
     gpu: bool = True
     command: str = "nvidia-smi -q -x"
+
+
+class Compression_Text(lib.DockerTask):
+    parallel: bool = False
+    priority: int = 1
+    image: str = "ghcr.io/sparecores/benchmark:main"
+    command: str = "bash -c 'cd /usr/local/turbobench; ./turbobench -v0 -eALL dickens | ansi2txt'"
