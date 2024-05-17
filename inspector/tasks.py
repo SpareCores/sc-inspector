b2c0b9e8b71d436eea785170e1465a4bc2edeafb
bra-fsn <nagy.attila@gmail.com>
2024-05-17 10:45:35 +0200

    Add lshw

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -8,3 +8,10 @@ class DmiDecode(lib.DockerTask):
     image: str = "ghcr.io/sparecores/dmidecode:main"
     parse_output: list = [parse.dmidecode]
     command: str = "dmidecode"
+
+
+class Lshw(lib.DockerTask):
+    parallel: bool = True
+    priority: int = 0
+    image: str = "ghcr.io/sparecores/hwinfo:main"
+    command: str = "lshw -json"
