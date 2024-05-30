dcb76da107a98000ea3c7cc60e739dd3b3a85231
bra-fsn <nagy.attila@gmail.com>
2024-05-30 18:06:28 +0200

    run

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -118,7 +118,7 @@ def start(ctx, exclude, start_only):
         # get default instance opts for the vendor and add ours
         instance_opts = default(getattr(sc_runner.resources, vendor).DEFAULTS, "instance_opts")
         instance_opts |= dict(user_data_base64=b64_user_data, key_name="spare-cores")
-        # runner.create(vendor, {}, RESOURCE_OPTS.get(vendor) | dict(instance=server, instance_opts=instance_opts))
+        runner.create(vendor, {}, RESOURCE_OPTS.get(vendor) | dict(instance=server, instance_opts=instance_opts))
 
 
 @cli.command()
