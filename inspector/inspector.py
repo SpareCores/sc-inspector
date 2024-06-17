31db4d118c738ed80b54fe3948eb14740abbea24
bra-fsn <nagy.attila@gmail.com>
2024-06-17 13:57:06 +0200

    Install nvidia container-toolkit

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -191,6 +191,13 @@ apt-get update -y >> /tmp/output 2>&1
 # https://ubuntu.com/server/docs/nvidia-drivers-installation
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin ubuntu-drivers-common >> /tmp/output 2>&1
 ubuntu-drivers install >> /tmp/output 2>&1
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list >> /tmp/output 2>&1
+apt-get update -y >> /tmp/output 2>&1
+apt-get install -y nvidia-container-toolkit >> /tmp/output 2>&1
+systemctl restart docker
 # stop some services to preserve memory
 snap stop amazon-ssm-agent >> /tmp/output 2>&1
 systemctl stop chrony acpid cron multipathd snapd systemd-timedated >> /tmp/output 2>&1
