f7f191aa79f8ade3aaa971859fa17043274863ea
bra-fsn <nagy.attila@gmail.com>
2024-07-10 11:13:54 +0200

    Don't fail on destroy 404

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -366,7 +366,11 @@ def start(ctx, exclude, start_only):
             for zone in zones:
                 resource_opts["zone"] = zone
                 # before starting, destroy everything to make sure the user-data will run (this is the first boot)
-                runner.destroy(vendor, {}, resource_opts | dict(instance=server))
+                try:
+                    runner.destroy(vendor, {}, resource_opts | dict(instance=server))
+                except Exception:
+                    # GCP fails on already deleted resource, don't fail on it
+                    logging.exception("Error while destroying stack")
                 try:
                     runner.create(vendor, {}, resource_opts | dict(instance=server, instance_opts=instance_opts))
                     break
