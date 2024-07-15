af9e90ba1080b470d6788467143f9d5109bcec26
bra-fsn <nagy.attila@gmail.com>
2024-07-15 23:21:14 +0200

    Use spot for GPU instances

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -210,7 +210,8 @@ def start(ctx, exclude, start_only):
             else:
                 bootdisk_init_opts |= dict(image="ubuntu-2404-lts-amd64", size=16)
 
-            is_preemptible = server.startswith("e2")
+            # e2 needs to be spot, also, we have only spot quotas for selected GPU instances
+            is_preemptible = server.startswith("e2") or gpu_count
             resource_opts |= dict(bootdisk_init_opts=bootdisk_init_opts,
                                   scheduling_opts=dict(
                                       preemptible=is_preemptible,
