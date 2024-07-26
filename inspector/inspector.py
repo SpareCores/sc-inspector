18a6c2371b9d8e1e042886cdaf8e5de3c37d9592
bra-fsn <nagy.attila@gmail.com>
2024-07-26 23:30:57 +0200

    back

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -273,7 +273,7 @@ def start(ctx, exclude, start_only):
                 )
                 lib.write_meta(meta, os.path.join(data_dir, task.name, lib.META_NAME))
             repo.push_path(data_dir, f"Failed to start server from {repo.gha_url()}")
-        # break
+        break
         # count += 1
         # if count == 3:
         #     break
