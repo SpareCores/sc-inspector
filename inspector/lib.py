fe0cb15f03d790bc713bb9c2a3fb369186bcb308
bra-fsn <nagy.attila@gmail.com>
2024-08-15 11:31:18 +0200

    update meta on skips

--- inspector/lib.py
+++ inspector/lib.py
@@ -320,6 +320,11 @@ def run_tasks(vendor, data_dir: str | os.PathLike, instance: str, gpu_count: int
     for taskgroup in sorted(taskgroups.keys()):
         for task in taskgroups[taskgroup]:
             if not should_run(task, data_dir, vendor, instance, gpu_count):
+                # update meta, so we have data
+                meta = Meta(start=datetime.now(), task_hash=task_hash(task))
+                meta.exit_code = -2
+                meta.error_msg = "Task should not be running on this instance"
+                write_meta(meta, os.path.join(data_dir, task.name, META_NAME))
                 continue
             logging.info(f"Starting {task.name}")
             q.put(task)
