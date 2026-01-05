d94975c17e53bbf6bd61c5e875c1099a31b05d63
bra-fsn <nagy.attila@gmail.com>
2026-01-05 21:19:23 +0100

    these machines die with membench as well soft lockup - CPU #XX stuck for XXXs

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -23,8 +23,6 @@ RUN_NEW_TASKS_ON_SERVERS = {
     ("azure", "M416s_9_v2"),
     ("gcp", "c3d-highcpu-360"),
     ("ovh", "a10-180"),
-    ("ovh", "c3-256"),
-    ("ovh", "l4-360"),
 }
 
 # get the amount of available memory
