b501c4d57de0db9ea9161442a6e936ab2a13ad68
bra-fsn <nagy.attila@gmail.com>
2026-02-12 15:33:50 +0100

    allow

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -373,7 +373,7 @@ def start(ctx, exclude, start_only, vendor):
             "ecs.c6r.large",
             "ecs.c6r.xlarge",
             "ecs.c7a.large",
-            # "ecs.sgn7i-vws-m2s.xlarge",
+            "ecs.sgn7i-vws-m2s.xlarge",
         }
         if vnd == "alicloud" and server not in alicloud_servers:
             logging.info(f"Excluding {vnd}/{server}")
