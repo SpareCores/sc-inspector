625c21645684cdd680d6799e402b41461a6edc05
bra-fsn <nagy.attila@gmail.com>
2026-02-26 09:34:18 +0100

    enable these vendors

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -191,7 +191,7 @@ RUN_NEW_TASKS_ON_SERVERS = DynamicServerSet(
         ("aws", "p6-b200.48xlarge"),
         ("aws", "p6-b300.48xlarge"),
     },
-    enable_vendors={"alicloud"},
+    enable_vendors={"alicloud", "gcp", "hcloud", "ovh", "upcloud"},
     include_fresh_servers=True,  # also include servers without dmidecode/meta.json (newly introduced)
 )
 
