5ee32c43ed1d554f5a5d9b466e4e6d8366b6ca06
bra-fsn <nagy.attila@gmail.com>
2026-03-04 23:00:30 +0100

    remove

--- inspector/tasks.py
+++ inspector/tasks.py
@@ -89,8 +89,6 @@ RUN_NEW_TASKS_ON_SERVERS = DynamicServerSet(
         ("gcp", "c2d-highmem-32"),
         ("hcloud", "ccx53"),
         ("ovh", "r3-256"),
-        ("azure", "Standard_L32aos_v4"),
-        ("alicloud", "ecs.r8ae.8xlarge"),
         # ("aws", "t3.nano"),
         # ("aws", "t3a.nano"),
         # ("aws", "i7ie.48xlarge"),
